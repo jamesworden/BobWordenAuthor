e1a8249f7f513e366c6468fd003d14e68b17aa72
jamesworden <jamesworden.ny@gmail.com>
2024-03-25 12:14:41 -0400

    Update footer text

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="flex justify-around py-4 bg-gray-800 text-gray-300">
   <div class="w-full px-1 flex gap-4 page-content-width flex-col md:flex-row">
-    <p class="footer-paragraph">
+    <p class="max-w-80">
       990 Stewart Avenue, 1st Floor
       <br />
       Garden City, NY 11530
@@ -13,12 +13,11 @@
     <p class="max-w-80">
       All mediation and arbitration services are coordinated through National
       Mediation and Arbitration.
-      <br />
-      <br />
-      No legal advice given without private retainer.
     </p>
 
-    <p class="footer-paragraph">
+    <p class="max-w-80">No legal advice given without private retainer.</p>
+
+    <p class="max-w-80">
       Copyright © 2024 Bob Worden Esq. All Rights Reserved.
     </p>
   </div>
