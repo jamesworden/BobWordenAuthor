6605cdbb0a5cd72a3f693a7d1cf39ec68671ed4e
jamesworden <jamesworden.ny@gmail.com>
2024-03-25 10:17:40 -0400

    Hero section work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,11 +13,33 @@
 <section>
   <ProfileCard />
 
-  <div>
-    <h3>Bob Worden's New Title</h3>
+  <div class="diagonal-bg p-8">
+    <div class="p-16 bg-gray-50">
+      <div class="mb-12">
+        <h3 class="uppercase text-gray-600 tracking-widest">
+          Bob Worden's New Title
+        </h3>
 
-    <h1>How to Stop Wars and Save the World</h1>
+        <h1>How to Stop Wars and Save the World</h1>
 
-    <h2>Lessons in Negotiation, Settlement, and Leadership</h2>
+        <h2>Lessons in Negotiation, Settlement, and Leadership</h2>
+      </div>
+
+      <button
+        class="text-white bg-amber-500 py-2 px-8 uppercase tracking-wide shadow-lg font-bold"
+        >Read More</button
+      >
+    </div>
   </div>
 </section>
+
+<style>
+  .diagonal-bg {
+    background-color: transparent;
+    background-image: -webkit-linear-gradient(
+      -50deg,
+      transparent 75%,
+      #e5e7eb 25%
+    );
+  }
+</style>
